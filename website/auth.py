8e68b32cdb5368ba056a4d48c0050fece0e7d600
jobanpreet <joban52@gmail.com>
2022-06-29 22:50:30 -0500

    update user login logic

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth',__name__)
 
@@ -14,6 +15,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category="success")
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Wrong Password', category ="error")
         else: 
@@ -21,8 +24,10 @@ def login():
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -48,6 +53,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account Created', category='success')
             return redirect(url_for("views.home"))
     return render_template("sign_up.html")
