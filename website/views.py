290f890de1a2af75d79476e527f454a94812ed30
jobanpreet <joban52@gmail.com>
2022-06-30 13:59:40 -0500

    Add note creation functionality

--- website/views.py
+++ website/views.py
@@ -1,9 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views',__name__)
 
-@views.route('/')
+@views.route('/',methods =['GET','POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        if current_user.is_authenticated:
+            data = request.form.get('note')
+            new_note = Note(data=data,user_id = current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note Saved', category="success")
+
     return render_template("home.html", user=current_user)
