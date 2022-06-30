d7f17f4383403003c9fe8daf28a31afa177eaf7a
jobanpreet <joban52@gmail.com>
2022-06-29 22:06:07 -0500

    Add user authentication validation

--- website/auth.py
+++ website/auth.py
@@ -7,6 +7,17 @@ auth = Blueprint('auth',__name__)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category="success")
+            else:
+                flash('Wrong Password', category ="error")
+        else: 
+            flash('No User Found', category ="error")
     return render_template("login.html")
 
 @auth.route('/logout')
