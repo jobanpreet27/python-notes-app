f4bbfbce253cf7ef61481ed5b276c718d954bec0
jobanpreet <joban52@gmail.com>
2022-07-01 17:16:04 -0500

    Fix database creation error

--- website/__init__.py
+++ website/__init__.py
@@ -34,5 +34,8 @@ def create_app():
 
 def create_database(app):
     if not path.exists('website/'+DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+        try:
+            db.create_all(app=app)
+            print('Created Database!')
+        except:
+            print("Exception occured")
